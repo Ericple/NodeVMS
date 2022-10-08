dfd9b10b436ac69c4db7575da1ffd8f12e602654
Ericple <dev@peercat.cn>
2022-10-08 08:47:40 +0800

    weekly routine updates

---
+++ hook.net/NodeVMS_Hook.cs
@@ -0,0 +1,19 @@
+﻿namespace hook.net;
+
+public class NodeVMS_Hook {
+    private string uri;
+    public
+//constructor
+    NodeVMS_Hook(string uri) {
+        this.uri = uri;
+    }
+//get or set hook uri
+    string URI(string uri) {
+        if (uri == "") {
+            return this.uri;
+        } else {
+            this.uri = uri;
+            return this.uri;
+        }
+    }
+} 
